e2964261e9e2c91bd0684b718244e8d090db82e0
jsfern774 <35469697+jsfern774@users.noreply.github.com>
2018-02-15 14:54:50 -0700

    Added functions

--- calculator/calc-javascript.js
+++ calculator/calc-javascript.js
@@ -1,3 +1,6 @@
+
+
+
 var display = "";
 var calculation = 0;
 
@@ -5,17 +8,31 @@ function number(value){
 
   if (value == '=' ){
       var calculation = eval(display);
-      document.getElementById("display").innerHTML = "=" + calculation;
+      document.getElementById("display").value = "=" + calculation;
 
-  } else if (value == "C"){
+  } else if (value == "AC"){
       display = "";
       calculation = 0;
-      document.getElementById("display").innerHTML = "";
+      document.getElementById("display").value = "";
+
+      // percent function
+  } else if (value == "%"){
+      display = "";
+      var percent = (value / display);
+      document.getElementById("display").value = sqrt;
+
+      // square root function
+  } else if (value == "squarer"){
+        display += "";
+        var sqrt = Math.sqrt(parseValue(display));
+        document.getElementById("display").value = display;
+
+
   }else {
       display += value;
       var pValue = parseFloat(value);
 
-      document.getElementById("display").innerHTML = display;
+      document.getElementById("display").value = display;
   }
 
 }
