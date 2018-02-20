8a97cfd39f7f754826d57fcb9259cb50c4d4bde7
Rusty-sk <35604274+Rusty-sk@users.noreply.github.com>
2018-02-20 15:38:14 -0700

    commit js

--- microwave/js/microwave.js
+++ microwave/js/microwave.js
@@ -1,10 +1,50 @@
 
-var display = "";
+var keypadDisplay =[];
+var clockDisplay = [];
+var timerDisplay = [];
 var time =0;
 var counter = 0;
 var timer;
 var myTime;
-   
+var keypad = "";
+var clockOn;
+var pCount; 
+// ****************
+//  display
+
+// ****************   
+
+function display() {
+    if (timerDisplay.length!=0){
+        
+        // document.getElementById("clockDisplay").innerHTML = timerDisplay[0] + ":" + timerDisplay[1];
+        
+        return;
+        
+    }else if(keypadDisplay.length != 0) {
+        if (keypadDisplay[1]<10){
+            document.getElementById("clockDisplay").innerHTML = keypadDisplay[0] + ":0" + keypadDisplay[1];
+        }else{
+            document.getElementById("clockDisplay").innerHTML = keypadDisplay[0] + ":" + keypadDisplay[1];
+        }
+
+        return;
+    }else{
+        clockOn = 1;
+        
+        if (clockDisplay[0]>12){
+            document.getElementById("clockDisplay").innerHTML = (clockDisplay[0]-12) + ":" + clockDisplay[1] + "pm";
+            startClock();
+
+        }else{
+            document.getElementById("clockDisplay").innerHTML = clockDisplay[0] + ":" + clockDisplay[1] +"am";
+            startClock();
+        }
+    }
+}
+
+
+
 
 // ****************
 //  Key pad
@@ -12,22 +52,52 @@ var myTime;
 // ****************
 
 function number(value){
-    
-    if (value == 'Start' ){
-        
 
-    } else if (value == "Clear"){
-        display = "";
-        time = 0;
-        document.getElementById("clockDisplay").innerHTML = "";
+    if (value == 'start' ){
+        if (keypadDisplay.length==0){
+            return;
+        }else{
+            clockOn = 0;
+            pCount=0;
+            if (timerDisplay.length==0){
+                keypad="";
+                timerDisplay = keypadDisplay;
+                keypadDisplay = [];
+                startTimer();
+                
+            }else{
+                startTimer();
+                return;
+            }
+        }
+    } else if (value == "clear"){
+        stopTimer();
+        return;
+    
+    } else if (keypad.length>3){
+        pCount=0;
+        value="";
+        return;
+
     }else {
-        display += value;
-        var pValue = parseFloat(value);
-        
-        document.getElementById("clockDisplay").innerHTML = display;
+        clockOn = 0;
+        pCount=0;
+        keypad += value;
+        var timeEntry= parseFloat(keypad);
+        if (keypad>100){
+            var minuteEntry;
+            minuteEntry = Math.trunc(timeEntry/100);
+            var secondsEntry = timeEntry - minuteEntry*100;
+            keypadDisplay = [minuteEntry, secondsEntry];
+            display();
+        }else{
+            keypadDisplay = [0, timeEntry];
+            display();
+        }
     }
 
 }
+
 
 // ****************
 //  Timer Section
@@ -36,20 +106,69 @@ function number(value){
 
 
 
-function startTimer() {
-    document.getElementById('clockDisplay').innerHTML ="<h1>" + counter--  + "</h1>";
-    document.getElementById('startBtn').disabled=true;
-    timer= setTimeout("startTimer()", 1000);
-
-}
-
-
 function stopTimer(){
-            clearTimeout(timer);
-            document.getElementById('startBtn').disabled = false;
+    pCount++;
+        if (pCount==2){
+            keypad = "";
+            time = 0;
+            keypadDisplay = [0, "00"];
+            display();
+            return;
+        }else if(pCount==3){
+            keypadDisplay = [];
+        }else{
 
-            counter=0;
+        }
 }
+
+
+
+
+function startTimer() {
+//   var presentTime = document.getElementById('clockDisplay').innerHTML;
+//   var timerDisplay = presentTime.split(/[:]+/);
+
+  if(timerDisplay[1]==-1 && timerDisplay[0]>0){
+    timerDisplay[0] --;
+    timerDisplay[1] = 59;
+    }
+    s = timerDisplay[1];
+    timerDisplay[1]--;
+    
+
+    if (s < 10 ) {
+        s = "0" + s;
+    }
+    
+    if (timerDisplay[0]==0 && timerDisplay[1]==0){
+        finished();
+    }else{
+
+       document.getElementById('clockDisplay').innerHTML =
+       timerDisplay[0] + ":" + s;
+    
+      setTimeout("startTimer()" , 1000);
+    }
+      
+}
+
+function finished() {
+
+    //  document.getElementById('clockDisplay').innerHTML = "DONE";
+    timerDisplay = []; 
+       var i=1;
+    setInterval(function(){
+            if (i<4){
+                document.getElementById('clockDisplay').innerHTML = "Done";
+                i++;
+            }else{
+                display();
+                }
+            
+            },3000);
+
+}
+
 
 
 
@@ -61,9 +180,13 @@ function stopTimer(){
 
 function startClock(){
 
-    getTime();
-
-    setInterval("getTime()",1000);
+    if(clockOn==0){
+        display();
+        return;
+    }else{
+    
+    setInterval("getTime()", 5000);
+    }
 }
 
 function getTime(){
@@ -75,22 +198,16 @@ function getTime(){
     var minutes = today.getMinutes();
     var seconds = today.getSeconds();
 
-    if(seconds<10){
-
-      seconds = '0' + seconds;
-    }
-
     if(minutes<10){
       minutes = '0' + minutes;
     }
 
-    if(hour<10){
-      hour = '0' + hour;
-    }
+    myTime = hour + ":" + minutes;
+    clockDisplay = myTime.split(/[:]+/);
+    display();
+    
 
-    myTime = hour + ":" + minutes + ":" + seconds;
-    document.getElementById('clock').value = myTime;
-    document.getElementById('demo').innerHTML = today;
+    
 
 }
 
