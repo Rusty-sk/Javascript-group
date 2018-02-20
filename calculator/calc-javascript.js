8fb125905c404b434c64bc6e1735c34b76a9be2a
jsfern774 <35469697+jsfern774@users.noreply.github.com>
2018-02-19 23:38:43 -0700

    Added more functions

--- calculator/calc-javascript.js
+++ calculator/calc-javascript.js
@@ -1,9 +1,9 @@
   
 
-var display = el("#display"), // The Calculator displayed
-    equals = el("#equals"), // Equal button
-    nums = el(".num"), // List of numbers
-    ops = el(".ops"), // List of operators
+var display = "#display", // The Calculator displayed
+    equals = "#equals", // Equal button
+    nums = ".num", // List of numbers
+    ops = ".ops", // List of operators
     newNum = "", // Current number
     oldNum = "", // First number
     resultNum, // Result
@@ -13,18 +13,18 @@ var display = el("#display"), // The Calculator displayed
 // When: Number is clicked. Get the current number selected
 function setNum() {
     if (resultNum) { // If a result was displayed, reset number
-      theNum = this.getAttribute("data-num");
+      newNum = this.getAttribute("data-num");
       resultNum = "";
     } else { // Otherwise, add digit to previous number (this is a string!)
-      theNum += this.getAttribute("data-num");
+      newNum += this.getAttribute("data-num");
     }
     display.innerHTML = newNum; // Display the current number
   };
 
 // When: Operator(+, -, /, x, etc.) is clicked. Pass number to oldNum and save operator
 function moveNum() {
-    oldNum = theNum;
-    theNum = "";
+    oldNum = newNum;
+    newNum = "";
     operator = this.getAttribute("data-ops");
 
     equals.setAttribute("data-result", ""); // Reset result in display
@@ -34,32 +34,32 @@ function moveNum() {
 //*****************
 
   // When: Equals is clicked. Calculate result
-function(displayNum) {
+function displayNum() {
 
     // Convert stores variables to raw numbers
     oldNum = parseFloat(oldNum);
-    theNum = parseFloat(theNum);
+    newNum = parseFloat(newNum);
 
     // Perform operation
     switch (operator) {
       case "plus":
-        resultNum = oldNum + theNum;
+        resultNum = oldNum + newNum;
         break;
 
       case "minus":
-        resultNum = oldNum - theNum;
+        resultNum = oldNum - newNum;
         break;
 
       case "times":
-        resultNum = oldNum * theNum;
+        resultNum = oldNum * newNum;
         break;
 
       case "divide":
-        resultNum = oldNum / theNum;
+        resultNum = oldNum / newNum;
         break;
 
       case "squared":
-        resultNum = oldNum **;
+        resultNum = oldNum * oldNum;
         break;
 
       case "sqrRoot":
@@ -67,32 +67,65 @@ function(displayNum) {
         break;
 
       case "percentage":
-        resultNum = oldNum * (theNum/100);
+        resultNum = oldNum * (newNum/100);
         break;
 
         // If equal is pressed without a new number
       default:
-        resultNum = theNum;
+        resultNum = newNum;
     }
-//////////////
+///////////////////
 
     // If NaN or Infinity returned
     if (!isFinite(resultNum)) {
       if (isNaN(resultNum)) { // If result is not a number; set off by, eg, double-clicking operators
-        resultNum = "You broke it!";
-      } else { // If result is infinity, set off by dividing by zero
-        resultNum = "Look at what you've done";
-        el('#calculator').classList.add("broken"); // Break calculator
-        el('#reset').classList.add("show"); // And show reset button
+        resultNum = "Error";
+      } else { // If result is infinity (dividing by zero)
+        resultNum = "No, No, No";
+        // We can add some cool function in place of the broken error message if we have some time
+        // el('#calculator').classList.add("broken"); // Break calculator
+        // el('#reset').classList.add("show"); // And show reset button
       }
     }
 
-    // Display result, finally!
-    viewer.innerHTML = resultNum;
+    // Code to display the result on the screen
+    display.innerHTML = resultNum;
     equals.setAttribute("data-result", resultNum);
 
-    // Now reset oldNum & keep result
+    // Resetting the old saved number and keeping the result waiting for next commands
     oldNum = 0;
-    theNum = resultNum;
+    newNum = resultNum;
 
   };
+
+// Clear (C) command
+function clearDisplay() {
+  oldNum = "";
+  newNum = "";
+  display.innerHTML = "0";
+  equals.setAttribute("data-result", resultNum);
+};
+
+// Button fuctionality 
+
+  // Add click event to numbers
+  for (var i = 0, l = nums.length; i < l; i++) {
+    nums[i].onclick = setNum();
+  }
+
+  // Add click event to operators
+  for (var i = 0, l = ops.length; i < l; i++) {
+    ops[i].onclick = moveNum();
+  }
+
+  // Add click event to equal sign
+  equals.onclick = displayNum();
+
+  // Add click event to clear button
+  el("#clear").onclick = clearDisplay();
+
+  // // Add click event to reset button
+  // el("#reset").onclick = function() {
+  //   window.location = window.location;
+  // };
+
