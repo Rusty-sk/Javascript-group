caa56177969f5879d8fee9de9dca325fe708a6e7
jsfern774 <35469697+jsfern774@users.noreply.github.com>
2018-02-21 12:07:30 -0700

    Fixed some bugs

--- calculator/calc-javascript.js
+++ calculator/calc-javascript.js
@@ -1,7 +1,6 @@
 // TO DO
 // character Limit for calculations(display/string length)
-// hide placeholder dots for funk display
-
+// hide placeholder dot for funk display
 
 // function to retrieve elements
 var el = function(element) {
@@ -11,6 +10,7 @@ var el = function(element) {
     return document.querySelectorAll(element); // Otherwise, returns a nodelist
   };
 
+// Global variables
 var display = el("#display"), // The Calculator displayed
     memDisplay = el("#memDisplay"), // The display for stored number
     equals = el("#equals"), // Equal button
@@ -18,6 +18,7 @@ var display = el("#display"), // The Calculator displayed
     sqrRoot = el("#sqrRoot"), // Square Root button
     percent = el("#percent"), // Square Root button
     plus = el("#plus"), // Plus Button
+    clear = el("#clear"), // AC button
     decimal = el("#decimal"), // Decimal Button
     nums = el(".num"), // List of numbers
     ops = el(".ops"), // List of operators
@@ -27,10 +28,12 @@ var display = el("#display"), // The Calculator displayed
     resultNum, // Result
     operator; // + - / * etc.
 
-// When: Number is clicked. Get the current number selected
+// When a number is clicked, Get the current number selected.
 var setNum = function() {
   if (newNum.length > maxNumLength) {
       newNum = "DANGER OVERLOAD x(";
+      resultNum = "DANGER OVERLOAD x(";
+      return;
     } else {
       if (resultNum) { // If a result was displayed, reset number
         newNum = this.getAttribute("data-num");
@@ -38,21 +41,21 @@ var setNum = function() {
         } else { // if within bounds will add digits to previous number
             newNum += this.getAttribute("data-num");
     }
-
     display.innerHTML = newNum; // Display the current number
     memDisplay.innerHTML = oldNum; // Display the number in memory area
   };
 };
 
-// When: Operator(+, -, /, x, etc.) is clicked. Pass number to oldNum and save operator
+// When: Operator(+, -, /, x, etc.) is clicked. Pass number to oldNum, save operator, and show in funk display
 var moveNum = function() {
     oldNum = newNum;
     newNum = "";
     operator = this.getAttribute("data-ops")
     funkDisplay.innerHTML = this.getAttribute("id");
-    equals.setAttribute("data-result", ""); // Reset result in display
+    equals.setAttribute("data-result", ""); // Reset the display
   };
 
+// Disable the decimal from being inputed more than once
 var disableDecimal = function(){
     newNum += this.getAttribute("data-num");
     decimal.disabled=true;
@@ -61,10 +64,10 @@ var disableDecimal = function(){
 //OPERATORS
 //*****************
 
-  // Equals button calculations
+  // Equals button
   var displayNum = function() {
 
-    // Convert stores variables to raw numbers
+    // Convert stored variables to raw numbers
     oldNum = parseFloat(oldNum);
     newNum = parseFloat(newNum);
 
@@ -93,26 +96,24 @@ var disableDecimal = function(){
 
     // If NaN or Infinity returned
     if (!isFinite(resultNum)) {
-      if (isNaN(resultNum)) { // If result is not a number; set off by, eg, double-clicking operators
+      if (isNaN(resultNum)) { // If result is not a number; set off by double-clicking operators, clicking only equals, etc.
         resultNum = "Error";
-      } else { // If result is infinity (dividing by zero)
-        resultNum = "No, No, No!!! 0w0";
+      } else { // If you should divide by zero
+        resultNum = "No, No, No!!! >:( ";
       }
     }
 
-    // Code to display the result on the screen
+    // Code to display results on the display and to reset stored numbers
     display.innerHTML = resultNum;
     memDisplay.innerHTML = "";
     funkDisplay.innerHTML = ".";
     equals.setAttribute("data-result", resultNum);
     decimal.disabled=false;
-
-    // Resetting the old saved number and keeping the result waiting for next commands
     oldNum = 0;
     newNum = resultNum;
   };
 
-//** Special function button calculations
+// Special function button calculations
 
 var displayRoot = function() {
 
@@ -186,7 +187,7 @@ newNum = parseFloat(newNum);
       }
 }
 
-// Clear button function
+// AC button 
 var clearDisplay = function() {
   oldNum = "";
   newNum = "";
@@ -197,7 +198,7 @@ var clearDisplay = function() {
   decimal.disabled=false;
 };
 
-// Button fuctionality and click events
+// click events for all buttons
 
   for (var i = 0, l = nums.length; i < l; i++) {
     nums[i].onclick = setNum;
@@ -217,4 +218,4 @@ var clearDisplay = function() {
 
   percent.onclick = displayPercent;
 
-  el("#clear").onclick = clearDisplay;
+  clear.onclick = clearDisplay;
